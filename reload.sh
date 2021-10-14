ae308af4be6cdd954a232b9acde336535990fbc9
tvanomr <tvano@mail.ru>
2021-10-14 16:50:52 +0300

    reload.sh fixed

--- reload.sh
+++ reload.sh
@@ -2,4 +2,4 @@
 git pull
 docker rm -f interviewgenerator
 docker build -t interviewgenerator .
-docker run -d --name interviewgenerator -e "VIRTUAL_HOST=interviewgenerator.dit.life" -e "VIRTUAL_PORT=24433" -e "LETSENCRYPT_HOST=interviewgenerator.dit.life" -e "LETSENCRYPT_EMAIL=s@oxton.ru" -v $(pwd)/logs:/var/log interviewgenerator
+docker run -d --name interviewgenerator -p 24433:24433 -e "VIRTUAL_HOST=interviewgenerator.dit.life" -e "VIRTUAL_PORT=24433" -e "LETSENCRYPT_HOST=interviewgenerator.dit.life" -e "LETSENCRYPT_EMAIL=s@oxton.ru" -v $(pwd)/logs:/var/log interviewgenerator --expose
